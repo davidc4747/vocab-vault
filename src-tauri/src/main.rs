f483ebccbe22354f900b62a214534de7add675fa
David G Chung <davidc4747@yahoo.com>
2024-08-28 12:24:13 -0700

    Start translating each morph one by one in a separate thread

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,12 +7,13 @@ mod morph;
 use morph::Morph;
 use serde::Deserialize;
 use std::str::FromStr;
+use std::thread;
 use std::{fs, sync::Mutex};
 use tauri::Manager;
 
-#[derive(Default)]
+#[derive(Debug, Default)]
 struct Store {
-    morph_list: Vec<Morph>,
+    morph_list: Vec<Mutex<Morph>>,
     index: Mutex<usize>,
 }
 
@@ -24,7 +25,8 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             // Grap Known Morphs
-            let known = fs::read_to_string("../public/known_morphs-2024-08-26@12-51-37.csv")?
+            let known = fs::read_to_string("../public/known_morphs-2024-08-26@12-51-37.csv")
+                .expect("know_morph csv file now found")
                 .split("\n")
                 .skip(1)
                 .filter_map(|text| Morph::from_str(text).ok())
@@ -32,8 +34,8 @@ fn main() {
                 .collect::<Vec<String>>();
 
             // Grab frequency file
-            let deepl_api_key = get_api_key().expect("Failed to load DeepL API key");
-            let mut morph_list = fs::read_to_string("../public/es-freq.csv")?
+            let morph_list = fs::read_to_string("../public/es-freq.csv")
+                .expect("frequency csv file now found")
                 .split("\n")
                 .skip(1)
                 .filter_map(|text| Morph::from_str(text).ok())
@@ -41,21 +43,29 @@ fn main() {
                     // make sure it's not in the "known.csv"
                     morph.lemma == morph.inflection && !known.contains(&morph.inflection)
                 })
-                .collect::<Vec<Morph>>();
+                .map(|m| Mutex::new(m.clone()))
+                .collect::<Vec<Mutex<Morph>>>();
 
-            // Request Translations for First few morphs
-            morph_list.iter_mut().take(2).for_each(|morph| {
-                let mut output = None;
-                tauri::async_runtime::block_on(async {
-                    output = deepl::translate(&deepl_api_key, &morph.inflection).await;
-                });
-                morph.english = output
-            });
-
-            // Initialize State
             app.manage(Store {
                 morph_list,
                 index: Mutex::new(0),
+            });
+
+            // Start Child Thread to request DeepL Transalations in the background
+            let handle = app.app_handle();
+            thread::spawn(move || {
+                let state = handle.state::<Store>().inner();
+
+                // Request Translations each morph
+                let deepl_api_key = get_api_key().expect("Failed to load DeepL API key");
+                state.morph_list.iter().for_each(|m| {
+                    let mut morph = m.lock().unwrap();
+                    tauri::async_runtime::block_on(async {
+                        let translation = deepl::translate(&deepl_api_key, &morph.inflection).await;
+                        println!("state: {translation:?}");
+                        morph.english = translation;
+                    });
+                });
             });
             Ok(())
         })
@@ -72,7 +82,7 @@ fn main() {
 fn next_morph(state: tauri::State<Store>) -> Option<Morph> {
     let mut index = state.index.lock().unwrap();
     *index += 1;
-    let next = state.morph_list.get(*index)?.clone();
+    let next = state.morph_list.get(*index)?.lock().ok()?.clone();
     Some(next)
 }
 
